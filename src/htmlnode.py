ba179f953c84035e4c989c1e6c2cfe9a22753272
ChuckieP <chuckporcelli@gmail.com>
2024-04-18 23:04:46 -0500

    HTMLNode unittests passed

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,12 +1,12 @@
 class HTMLNode:
-    def __init__(self, tag, value, children, props):
+    def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
         self.value = value
         self.children = children
         self.props = props
     
     def to_html(self):
-        raise NotImplementedError
+        raise NotImplementedError("to_html method not implemented")
     
     def props_to_html(self):
         props_list = []
@@ -14,3 +14,6 @@ class HTMLNode:
             # i = key, self.props[i] = value
             props_list.append(f" {i}=\"{self.props[i]}\"")
         return "".join(props_list)
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
