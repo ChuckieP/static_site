51592f6dcca274480cff80127661c9f563725d29
ChuckieP <chuckporcelli@gmail.com>
2024-04-19 11:29:52 -0500

    completed LeafNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -4,32 +4,40 @@ class HTMLNode:
         self.value = value
         self.children = children
         self.props = props
-    
+
     def to_html(self):
         raise NotImplementedError("to_html method not implemented")
-    
+
     def props_to_html(self):
-        props_list = []
-        for i in self.props:
-            # i = key, self.props[i] = value
-            props_list.append(f" {i}=\"{self.props[i]}\"")
-        return "".join(props_list)
+        if self.props is None:
+            return ""
+        props_html = ""
+        for prop in self.props:
+            props_html += f' {prop}="{self.props[prop]}"'
+        return props_html
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
     
 
 class LeafNode(HTMLNode):
-    def __init__(self, tag=None, value, children=None, props=None):
-        super().__init__(tag, value, children, props)
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
 
     def to_html(self):
-        if self.value == None:
-            raise ValueError("LeafNode does not have a value")
-        if self.tag == None:
+        if self.value is None:
+            raise ValueError("Invalid HTML: no value")
+        if self.tag is None:
             return self.value
-        leaf_html = ""
-        leaf_html += f"<{self.tag}>"
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
 
-# ^^^ PICK BACK UP HERE ^^^
-# https://www.boot.dev/assignments/ac96cd47-bf01-4599-8291-cd69534f288f
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+# vvvv manual testing stuff below vvvv
+""" tester1 = LeafNode("p", "This is a paragraph of text.")
+tester2 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+tester3 = LeafNode(None, "This is a paragraph of text.")
+print(tester1.to_html())
+print(tester2.to_html())
+print(tester3.to_html()) """
